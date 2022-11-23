87b0c0f5ee0e857c552256a37ddc4744452c011d
gusalburk <gusalbukrk@gmail.com>
2022-11-23 14:12:14 -0300

    require_once

--- www/index.php
+++ www/index.php
@@ -14,7 +14,7 @@
   $path = $_SERVER['REQUEST_URI'];
 
   if (count($domain) > 3) {
-    require __DIR__ . '/views/404.php';
+    require_once __DIR__ . '/views/404.php';
     exit();
   }
 
@@ -27,14 +27,14 @@
   switch ($path) {
     case "":
     case "/":
-      require __DIR__ . '/views/index.php';
+      require_once __DIR__ . '/views/index.php';
       break;
     case "/about":
-      require __DIR__ . '/views/about.php';
+      require_once __DIR__ . '/views/about.php';
       break;
     default:
       http_response_code(404);
-      require __DIR__ . '/views/404.php';
+      require_once __DIR__ . '/views/404.php';
   }
 
   ?>
