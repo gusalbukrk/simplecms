2b4d022ef2d4adbb7515eb7f7c1af0345f7addcb
gusalburk <gusalbukrk@gmail.com>
2022-11-26 23:39:44 -0300

    add subdomains list to index

--- www/index.php
+++ www/index.php
@@ -5,37 +5,24 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>simpleCMS</title>
 </head>
 
 <body>
   <?php
   $domain = explode(".", $_SERVER['HTTP_HOST']);
-  $path = $_SERVER['REQUEST_URI'];
 
+  // allow only one subdomain level
   if (count($domain) > 3) {
     require_once __DIR__ . '/views/404.php';
     exit();
   }
 
-  $subdomain = count($domain) == 2 || $domain[0] == "www" ? "" : $domain[0];
-
   echo var_dump($domain) . "</br>";
-  echo $path . "</br>";
+  $path = $_SERVER['REQUEST_URI'];
+  $subdomain = (count($domain) == 2 || $domain[0] == "www") ? "" : $domain[0];
+  echo "path: $path</br>";
   echo "subdomain: $subdomain</br>";
-
-  $servername = "db";
-  $username = "admin";
-  $password = "adminpw";
-  $port = "3306";
-  //
-  try {
-    $conn = new PDO("mysql:host=$servername;port=$port", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected succesfully";
-  } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
 
   switch ($path) {
     case "":
