3766d8ac039a5233533e8c93e84685ae55324a50
gusalburk <gusalbukrk@gmail.com>
2022-11-26 23:39:44 -0300

    add subdomains list to index

--- www/views/index.php
+++ www/views/index.php
@@ -1,15 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<h1>index</h1>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+<?php
+// every MySQL has following databases by default
+define("DEFAULT_DBS", ["information_schema", "mysql", "performance_schema", "sys"]);
 
-<body>
-  <h1>index</h1>
-</body>
+$servername = "db";
+$username = "root";
+$password = "rootpw";
+$port = "3306";
 
-</html>
+try {
+  $conn = new PDO("mysql:host=$servername;port=$port", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully</br>";
+
+  $dbs = [];
+
+  // populate $dbs with all non-default databases
+  $stmt = $conn->prepare('SHOW DATABASES');
+  $stmt->execute();
+  while (($db = $stmt->fetchColumn()) !== false) {
+    if (!in_array($db, DEFAULT_DBS)) array_push($dbs, $db);
+  }
+
+  // dev environment = 'online', production environment is 'site'
+  $tld = end(explode(".", $_SERVER["HTTP_HOST"]));
+
+  echo "<ul>";
+  foreach ($dbs as $index => $db) {
+    $address = "$db.simplecms.$tld";
+
+    echo "<li><a href=\"http://$address\">$db</a></li>";
+  }
+  echo "</ul>";
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
