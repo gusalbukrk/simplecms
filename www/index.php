b61c9b6b75ee69b65039d32e0f81a39ec9210906
gusalburk <gusalbukrk@gmail.com>
2022-11-30 23:14:25 -0300

    install bootstrap

--- www/index.php
+++ www/index.php
@@ -20,6 +20,7 @@ session_start();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
   <title>simpleCMS</title>
 </head>
 
