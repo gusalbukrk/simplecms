9e669309efb07b5ab7dd9b7a4034c505a7ea07a1
gusalbukrk <gusalbukrk@gmail.com>
2022-12-07 15:58:53 +0000

    separate `Table` & `User` views

--- www/Core/View.php
+++ www/Core/View.php
@@ -5,12 +5,14 @@ namespace Core;
 // every function inside view is called by a controller method of the same name
 class View
 {
+  protected $controller;
+
   public function __call($method, $args)
   {
     $method = preg_replace("/_/", "-", $method); // replace slash with underline
 
     require_once __DIR__ . "/../views/header.php";
-    require_once __DIR__ . "/../views/$method.php";
+    require_once __DIR__ . "/../views/$this->controller/$method.php";
     require_once __DIR__ . "/../views/footer.php";
   }
 }
--- www/User/View.php
+++ www/User/View.php
@@ -6,4 +6,8 @@ require_once __DIR__ . "/../Core/View.php";
 
 class View extends \Core\View
 {
+  public function __construct()
+  {
+    $this->controller = "User";
+  }
 }
