91e40ae1e7d92657c0e1bbef6371aa91ae3b5c30
gusalbukrk <gusalbukrk@gmail.com>
2022-12-07 21:42:40 +0000

    remove `login` link from `reset-password`

--- www/views/User/reset-password.php
+++ www/views/User/reset-password.php
@@ -6,7 +6,4 @@
   <div class="row mb-3">
     <input class="btn btn-primary" type="submit" name="action" value="Reset password">
   </div>
-  <p class="row text-end">
-    <a class="px-0" href="login">Log in</a>
-  </p>
 </form>
