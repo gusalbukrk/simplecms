46c84d91125564abf4c6bd4b6ea8db347e8d4711
gusalbukrk <gusalbukrk@gmail.com>
2023-02-27 18:28:19 +0000

    add toggler to password input at `User`

--- www/views/User/login.php
+++ www/views/User/login.php
@@ -5,10 +5,27 @@
   </div>
   <div class="mb-3">
     <label for="password" class="form-label fs-dot9 fw-bold text-dark-gray">Password</label>
-    <input type="password" name="password" id="password" class="form-control border-dark border-opacity-50 border-width-2" required>
+    <div class="input-group">
+      <input type="password" name="password" id="password" class="form-control border-dark border-opacity-50 border-width-2" required>
+      <span id="toggler" class="input-group-text border-dark border-opacity-50 border-width-2 bg-body border-left-0 cursor-pointer">
+        <i class="fa-solid fa-fw fa-eye"></i>
+      </span>
+    </div>
   </div>
   <div class="form-text mb-3">
     <a href="reset-password">Reset password</a>
   </div>
   <input type="submit" name="action" value="log in" class="btn btn-primary fw-bold">
 </form>
+<script>
+  const input = document.getElementById('password');
+  const toggler = document.getElementById('toggler');
+  const icon = toggler.querySelector('i');
+
+  toggler.addEventListener('click', e => {
+    icon.classList.toggle('fa-eye');
+    icon.classList.toggle('fa-eye-slash');
+
+    input.type = input.type === 'password' ? 'text' : 'password';
+  })
+</script>
--- www/views/User/signup.php
+++ www/views/User/signup.php
@@ -3,11 +3,26 @@
     <label for="email" class="form-label fs-dot9 fw-bold text-dark-gray">Email</label>
     <input type="email" name="email" id="email" class="form-control border-dark border-opacity-50 border-width-2" required>
   </div>
-
   <div class="mb-3">
     <label for="password" class="form-label fs-dot9 fw-bold text-dark-gray">Password</label>
-    <input type="password" name="password" id="password" class="form-control border-dark border-opacity-50 border-width-2" required>
+    <div class="input-group">
+      <input type="password" name="password" id="password" class="form-control border-dark border-opacity-50 border-width-2" required>
+      <span id="toggler" class="input-group-text border-dark border-opacity-50 border-width-2 bg-body border-left-0 cursor-pointer">
+        <i class="fa-solid fa-fw fa-eye"></i>
+      </span>
+    </div>
   </div>
-
   <input type="submit" name="action" value="sign up" class="btn btn-primary fw-bold">
 </form>
+<script>
+  const input = document.getElementById('password');
+  const toggler = document.getElementById('toggler');
+  const icon = toggler.querySelector('i');
+
+  toggler.addEventListener('click', e => {
+    icon.classList.toggle('fa-eye');
+    icon.classList.toggle('fa-eye-slash');
+
+    input.type = input.type === 'password' ? 'text' : 'password';
+  })
+</script>
